d03274d4f83490a96925c6d9ff596d923895c829
meghraj88 <79048066+meghraj88@users.noreply.github.com>
2025-08-24 10:09:13 +0000

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    optimizePackageImports: ['lucide-react'],
-  },
-  webpack: (config) => {
-    config.cache = false;
-    return config;
-  },
-}
+  output: 'standalone',
+};
 
-export default nextConfig
+export default nextConfig;
